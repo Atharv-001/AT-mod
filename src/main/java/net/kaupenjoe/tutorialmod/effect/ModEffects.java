260b467744d1a952af1114ca38acad0973db4779
Atharv-001 <atharv.coder28@gmail.com>
2025-03-22 21:22:33 +0530

    Update ModEffects.java

--- src/main/java/net/kaupenjoe/tutorialmod/effect/ModEffects.java
+++ src/main/java/net/kaupenjoe/tutorialmod/effect/ModEffects.java
@@ -11,12 +11,11 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
 public class ModEffects {
-    public static final RegistryEntry<StatusEffect> SLIMEY = registerStatusEffect("slimey",
-            new SlimeyEffect(StatusEffectCategory.NEUTRAL, 0x36ebab)
-                    .addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED,
-                            Identifier.of(TutorialMod.MOD_ID, "slimey"), -0.25f,
-                            EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
-
+    public static final RegistryEntry<StatusEffect> BLOOD_RUSH = registerStatusEffect("blood_rush",
+            new BloodRushEffect(StatusEffectCategory.BENEFICIAL, 0xFF0000) // Red color for blood
+                    .addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE, // Strength effect
+                            Identifier.of(TutorialMod.MOD_ID, "blood_rush"), 4.0,
+                            EntityAttributeModifier.Operation.ADDITION)); // Strength IV
 
     private static RegistryEntry<StatusEffect> registerStatusEffect(String name, StatusEffect statusEffect) {
         return Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(TutorialMod.MOD_ID, name), statusEffect);
