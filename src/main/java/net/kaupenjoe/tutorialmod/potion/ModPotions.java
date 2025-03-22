352ae4429f60ef409ca3e95ce023258a985f2efa
Atharv-001 <atharv.coder28@gmail.com>
2025-03-22 21:24:05 +0530

    Update ModPotions.java

--- src/main/java/net/kaupenjoe/tutorialmod/potion/ModPotions.java
+++ src/main/java/net/kaupenjoe/tutorialmod/potion/ModPotions.java
@@ -10,9 +10,8 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
 public class ModPotions {
-    public static final RegistryEntry<Potion> SLIMEY_POTION = registerPotion("slimey_potion",
-            new Potion(new StatusEffectInstance(ModEffects.SLIMEY, 1200, 0)));
-
+    public static final RegistryEntry<Potion> BLOOD_RUSH_POTION = registerPotion("blood_rush_potion",
+            new Potion(new StatusEffectInstance(ModEffects.BLOOD_RUSH, 1200, 3))); // Strength IV (level 3)
 
     private static RegistryEntry<Potion> registerPotion(String name, Potion potion) {
         return Registry.registerReference(Registries.POTION, Identifier.of(TutorialMod.MOD_ID, name), potion);
